3edd787d43407bb9fd50daa0e90471e9b12f3617
k-hongkham <k.hongkham@gmail.com>
2022-06-24 09:23:48 -0400

    starting Navbar component.

--- src/components/NAVBAR/Navbar.jsx
+++ src/components/NAVBAR/Navbar.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import useAuth from "../hooks/userAuth";
 
 const Navbar = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const { user } = useAuth();
+
   return <div></div>;
 };
 
