b274af5f55537a8dda76ea8dbe279d8c7cbaebd0
k-hongkham <k.hongkham@gmail.com>
2022-06-27 08:46:35 -0400

    added bootstrap to navbar - projects

--- src/components/NAVBAR/Navbar.jsx
+++ src/components/NAVBAR/Navbar.jsx
@@ -23,7 +23,14 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarsExample04">
           <ul className="navbar-nav me-auto mb-2 mb-md-0">
             <li className="nav-item dropdown">
-              <Link to="/Projects" style={{ textDecoration: "none" }}>
+              <Link
+                to="/Projects"
+                style={{ textDecoration: "none" }}
+                className="nav-link dropdown-toggle"
+                id="dropdown04"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 Projects
               </Link>
             </li>
