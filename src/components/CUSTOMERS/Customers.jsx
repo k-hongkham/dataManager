818d5f69d50ebc0b9bcd3b1c580380b2612e1a79
k-hongkham <k.hongkham@gmail.com>
2022-06-29 14:24:47 -0400

    customers now renders on customer tab, but no customers are shown.

--- src/components/CUSTOMERS/Customers.jsx
+++ src/components/CUSTOMERS/Customers.jsx
@@ -4,7 +4,6 @@ import useAuth from "../hooks/userAuth";
 import useLogin from "../hooks/useLogin";
 
 import { getAllCustomers } from "../../axios";
-import { Customer } from "../../../db";
 
 const Customers = () => {
   const { error, setError, errorMessage, setErrorMessage } = useLogin();
@@ -15,7 +14,6 @@ const Customers = () => {
       <div className="my-3 p-3 bg-body rounded shadow-sm">
         <h6 className="border-bottom pb-2 mb-0">Current Customers</h6>
         {allCustomers.map((_customer, idx) => {
-          if (_customer.id === Customer.id) return null;
           return (
             <div
               className="d-flex text-muted pt-3"
