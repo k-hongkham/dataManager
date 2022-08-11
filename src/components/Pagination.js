a4dbac546d555bfe4a28c25d45a453e115bd4084
k-hongkham <k.hongkham@gmail.com>
2022-08-11 11:12:32 -0400

    limit page numbers to 5.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Pagination = ({ listItemsPerPage, totalUsers, setCurrentPage }) => {
   const pageNumbers = [];
+
+  const [pageNumberLimit, setPageNumberLimit] = useState(5);
+  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
+  const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
+
   const paginate = (e, pageNumber) => {
     e.preventDefault();
     setCurrentPage(pageNumber);
@@ -13,17 +18,23 @@ const Pagination = ({ listItemsPerPage, totalUsers, setCurrentPage }) => {
   return (
     <nav>
       <ul className="pagination">
-        {pageNumbers.map((number) => (
-          <li key={number} className="page-item">
-            <a
-              onClick={(e) => paginate(e, number)}
-              href="!#"
-              className="page-link"
-            >
-              {number}
-            </a>
-          </li>
-        ))}
+        {pageNumbers.map((number) => {
+          if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+            return (
+              <li key={number} className="page-item">
+                <a
+                  onClick={(e) => paginate(e, number)}
+                  href="!#"
+                  className="page-link"
+                >
+                  {number}
+                </a>
+              </li>
+            );
+          } else {
+            return null;
+          }
+        })}
       </ul>
     </nav>
   );
