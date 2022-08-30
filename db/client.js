e4a18e64753492d364919045c95ee9039900f721
k-hongkham <k.hongkham@gmail.com>
2022-08-30 10:09:41 -0400

    updatged private to false.

--- db/client.js
+++ db/client.js
@@ -2,7 +2,7 @@
 const { Client } = require("pg");
 
 // change the DB_NAME string to whatever your group decides on
-const DB_NAME = "projectManager";
+const DB_NAME = "data-info-management";
 
 const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
