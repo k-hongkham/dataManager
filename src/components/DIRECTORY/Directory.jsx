75e2fbd5b73c20d26536a6775730e0e298bb1583
k-hongkham <k.hongkham@gmail.com>
2022-09-09 11:44:03 -0400

    adjusted directory cards.

--- src/components/DIRECTORY/Directory.jsx
+++ src/components/DIRECTORY/Directory.jsx
@@ -28,45 +28,48 @@ const Directory = ({
 
   return (
     <div className="container mx-auto pb-3 mb-3 mb-md-5 mt-4">
-      <h6 className="border-bottom pb-2 mb-0"> Company Directory</h6>
+      <Button variant="primary">New Contact +</Button>
+      <div className="my-3 p-3 bg-body rounded shadow-sm">
+        <h6 className="border-bottom pb-2 mb-0"> Company Directory</h6>
 
-      <div className="row justify-content-center mx-auto pb-3 mb-3 mb-md-5 mt-4">
-        {contactsList.map((contact, idx) => {
-          return (
-            <div
-              key={`allContacts: ${idx}`}
-              className="col-sm-6 col-md-4 col-xl-3 mb-3 me-md-3  "
-              style={{ border: "1px solid black" }}
-            >
-              <ContactCard contact={contact} />
-              <Button
-                variant="info"
-                onClick={(e) => {
-                  handleUpdateModalOpen(e, contact);
-                }}
+        <div className="row justify-content-center mx-auto pb-3 mb-3 mb-md-5 mt-4">
+          {contactsList.map((contact, idx) => {
+            return (
+              <div
+                key={`allContacts: ${idx}`}
+                className="col-sm-6 col-md-4 col-xl-3 mb-3 me-md-3  "
+                style={{ border: "1px solid black" }}
               >
-                Update {contact.id}
-              </Button>
-              {userEditModal ? (
-                <UpdateContact
-                  contact={contact}
-                  userEditModal={userEditModal}
-                  setUserEditModal={setUserEditModal}
-                  currentDirectoryContact={currentDirectoryContact}
-                  contactsList={contactsList}
-                  setContactsList={setContactsList}
-                />
-              ) : null}
-            </div>
-          );
-        })}
+                <ContactCard contact={contact} />
+                <Button
+                  variant="info"
+                  onClick={(e) => {
+                    handleUpdateModalOpen(e, contact);
+                  }}
+                >
+                  Update {contact.id}
+                </Button>
+                {userEditModal ? (
+                  <UpdateContact
+                    contact={contact}
+                    userEditModal={userEditModal}
+                    setUserEditModal={setUserEditModal}
+                    currentDirectoryContact={currentDirectoryContact}
+                    contactsList={contactsList}
+                    setContactsList={setContactsList}
+                  />
+                ) : null}
+              </div>
+            );
+          })}
+        </div>
       </div>
       <Pagination
-        listItemsPerPage={listItemsPerPage}
-        totalUsers={totalUsers}
-        setCurrentPage={setCurrentPage}
         currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        listItemsPerPage={listItemsPerPage}
         setListItemsPerPage={setListItemsPerPage}
+        totalUsers={totalUsers}
       />
     </div>
   );
